f423b7bfd396e1d7f9da33a5c02c85dae0555c03
ZhangFengyi <someonenamedzfy@vip.163.com>
2020-07-15 23:49:15 +0800

    to local
    to local

--- src/com/zhangfy/github/test/Test01.java
+++ src/com/zhangfy/github/test/Test01.java
@@ -5,6 +5,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("Hello,GIthub服务器");
 		System.out.println("Hello,GIthub服务器02");
+		System.out.println("GIthub服务器 response to local");
 	}
 
 }
